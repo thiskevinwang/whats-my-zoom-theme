367c4b5ff205f270140727a8b3b3c828daba4031
Kevin Wang <kwangsan@gmail.com>
2020-05-18 19:27:49 -0400

    use basic copy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,31 @@
 import styled from "styled-components";
 
-const Errthang = styled.div``;
+const Errthang = styled.div`
+  top: 40vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  max-width: 800px;
+  margin: auto;
+`;
+
 export default function Home() {
-  return <Errthang>What's my Zoom theme?</Errthang>;
+  return (
+    <Errthang>
+      <h1>What's my Zoom theme?</h1>
+
+      <details>
+        <summary>What is this?</summary>
+        <p>
+          This is a fun little tool to help you pick a theme for your next zoom
+          meeting! This theme could be anything from a background picture of
+          your favorite kind of animal, or a fun yoga position for you and your
+          team!
+        </p>
+      </details>
+    </Errthang>
+  );
 }
